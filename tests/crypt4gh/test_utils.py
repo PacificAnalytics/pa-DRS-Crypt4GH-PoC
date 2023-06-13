2bc5fb531d4b101256a449555acc66845926cba8
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2023-06-13 22:49:28 +0900

    maint: add file utils.

---
+++ tests/crypt4gh/test_utils.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from drs_filer.crypt4gh.utils import create_unique_filename, temp_folder
+
+
+def test_temp_folder():
+    old_cwd = Path.cwd()
+    with temp_folder() as d:
+        assert Path.cwd() == old_cwd
+        assert d.exists()
+    assert not d.exists()
+
+
+def test_create_unique_filename():
+    p = Path("/a/b/c")
+    f1 = create_unique_filename(p)
+    assert f1.parent == p
+
+    f2 = create_unique_filename(p)
+    assert f2 != f1
